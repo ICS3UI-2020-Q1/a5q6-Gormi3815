6091921a32197ceb3af41348a63b5ebc9db11001
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-09 12:13:40 +0000

    the program will make a square to the dimensions givin by the user

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
-/**
+import java.util.Scanner;
+/** This program will make a square to the dimesnsions that the user puts in
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
-
+// make an integer that prints based off how many
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner (System.in);
+    System.out.println("Please enter a number from 1-10");
+    int num1 = input.nextInt();
     
+  for  (int count = 0; count < num1 ; count++){
+    int star = count;
+    for (int count2 = 0; count2 <= num1 - 1 ; count2++){
+      int uyo = count2*num1;
+    System.out.print("*");
+    }
+    System.out.println();
+    
+    
+    
+    
+    }
   }
 }
